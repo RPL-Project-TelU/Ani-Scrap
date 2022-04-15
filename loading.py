7dc25a3b08d904c445ffb00da0da276633a54dc6
ComicalBanana <angelinayessyca@student.telkomuniversity.ac.id>
2022-04-15 20:14:20 +0700

    Loading

---
+++ loading.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designercVYbyh.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(492, 390)
+        self.radioButton = QRadioButton(Dialog)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(50, 110, 121, 41))
+        self.radioButton_3 = QRadioButton(Dialog)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.radioButton_3.setGeometry(QRect(50, 140, 121, 41))
+        self.radioButton_4 = QRadioButton(Dialog)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setGeometry(QRect(280, 110, 121, 41))
+        self.radioButton_5 = QRadioButton(Dialog)
+        self.radioButton_5.setObjectName(u"radioButton_5")
+        self.radioButton_5.setGeometry(QRect(280, 140, 121, 41))
+        self.radioButton_2 = QRadioButton(Dialog)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setGeometry(QRect(180, 200, 121, 41))
+        self.radioButton_6 = QRadioButton(Dialog)
+        self.radioButton_6.setObjectName(u"radioButton_6")
+        self.radioButton_6.setGeometry(QRect(180, 230, 121, 41))
+        self.pushButton_2 = QPushButton(Dialog)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(170, 20, 101, 101))
+        icon = QIcon()
+        icon.addFile(u"../../../../../../../../OneDrive/Pictures/497c63d793b1b497005bbec9f97e6c76.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_2.setIcon(icon)
+        self.pushButton_2.setIconSize(QSize(100, 100))
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(0, 100, 301, 291))
+        icon1 = QIcon()
+        icon1.addFile(u"../../../../../../../../OneDrive/Pictures/10756508.jpg", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon1)
+        self.pushButton.setIconSize(QSize(300, 300))
+        self.pushButton_3 = QPushButton(Dialog)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(190, 100, 301, 291))
+        self.pushButton_3.setIcon(icon1)
+        self.pushButton_3.setIconSize(QSize(300, 300))
+        self.pushButton_4 = QPushButton(Dialog)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(0, 0, 301, 291))
+        self.pushButton_4.setIcon(icon1)
+        self.pushButton_4.setIconSize(QSize(300, 300))
+        self.pushButton_5 = QPushButton(Dialog)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(190, 0, 301, 291))
+        self.pushButton_5.setIcon(icon1)
+        self.pushButton_5.setIconSize(QSize(300, 300))
+        self.pushButton_6 = QPushButton(Dialog)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(30, 80, 75, 23))
+        self.pushButton_5.raise_()
+        self.pushButton_4.raise_()
+        self.pushButton_3.raise_()
+        self.pushButton.raise_()
+        self.radioButton.raise_()
+        self.radioButton_3.raise_()
+        self.radioButton_4.raise_()
+        self.radioButton_5.raise_()
+        self.radioButton_2.raise_()
+        self.radioButton_6.raise_()
+        self.pushButton_2.raise_()
+        self.pushButton_6.raise_()
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.radioButton.setText(QCoreApplication.translate("Dialog", u"480p uservideo.xyz", None))
+        self.radioButton_3.setText(QCoreApplication.translate("Dialog", u"720p uservideo.xyz", None))
+        self.radioButton_4.setText(QCoreApplication.translate("Dialog", u"480p naniplay.com", None))
+        self.radioButton_5.setText(QCoreApplication.translate("Dialog", u"720p naniplay.com", None))
+        self.radioButton_2.setText(QCoreApplication.translate("Dialog", u"480p miuiku.com", None))
+        self.radioButton_6.setText(QCoreApplication.translate("Dialog", u"720p miuiku.com", None))
+        self.pushButton_2.setText("")
+        self.pushButton.setText("")
+        self.pushButton_3.setText("")
+        self.pushButton_4.setText("")
+        self.pushButton_5.setText("")
+        self.pushButton_6.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+    # retranslateUi
+
