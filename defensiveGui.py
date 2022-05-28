d30c7a9112c18a08ae92677bbcf40170e6a704cf
ComicalBanana <angelinayessyca@student.telkomuniversity.ac.id>
2022-05-28 21:13:03 +0700

    Memperbaiki defensiveGui.py

--- defensiveGui.py
+++ defensiveGui.py
@@ -1,19 +1,15 @@
 def btnSearchClick(self):
-    try:
-        query = self.textEdit.toPlainText()
-        self.Search = QtWidgets.QWidget()
-        ui = searchGui.Ui_Form()
-        ui.setupUi(self.Search)
-    except NameError:
-        import sys
-        app = QtWidgets.QApplication(sys.argv)
-        Dialog = QtWidgets.QDialog()
-        ui = Ui_Dialog()
-        ui.setupUi(Dialog)
-        Dialog.show()
-        sys.exit(app.exec_())
-        retranslateUi(self, Dialog)
-        Dialog.show()
-    else:
-        ui.updateList(webScrapper.querySearch(query))
-        self.Search.show()
+    query = self.textEdit.toPlainText()
+    if query == "":
+        self.warnDialog = QtWidgets.QDialog()
+        uiDialog = warnDialog.Ui_form()
+        uiDialog.setupUI(self.warnDialog)
+        self.warnDialog.show()
+        return 
+    self.Search = QtWidgets.QWidget()
+    ui = searchGui.Ui_Form()
+    ui.setupUi(self.Search)
+    ui.updateList(webScrapper.querySearch(query))
+    self.Search.show()
+        
+
